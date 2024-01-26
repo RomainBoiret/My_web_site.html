d7c94b77367df7050d70a0eae1f2ff1103de4f13
RomainBoiret <romainboiret@gmail.com>
2024-01-26 00:09:33 -0500

    Big change

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,38 @@
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+// toogle icon navigation
+let menuIcon = document.querySelector('#menu-icon');
+let navigation = document.querySelector('.navigation');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navigation.classList.toggle('active');
+}
+
+// remove toggle icon and navbar when click navigation links (scroll)
+menuIcon.classList.remove('bx-x');
+navigation.classList.remove('active');
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navigation links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
