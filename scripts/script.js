541bd2b41ccc350dab003ad5d7fc14f5034d97cb
Romain <romainboiret@gmail.com>
2024-01-29 19:23:34 -0500

    some change

--- scripts/script.js
+++ scripts/script.js
@@ -42,7 +42,7 @@ let main = document.getElementById('color-btn-1');
 main.onclick = function () {
     document.body.classList.add('main-theme');
     document.body.classList.remove('ocean-theme');
-    document.body.classList.remove('e-theme');
+    document.body.classList.remove('savane-theme');
     document.body.classList.remove('white-theme');
     document.body.classList.remove('discord-theme');
 }
@@ -51,14 +51,14 @@ let ocean = document.getElementById('color-btn-2');
 ocean.onclick = function () {
     document.body.classList.add('ocean-theme');
     document.body.classList.remove('main-theme');
-    document.body.classList.remove('e-theme');
+    document.body.classList.remove('savane-theme');
     document.body.classList.remove('white-theme');
     document.body.classList.remove('discord-theme');
 }
 
 let e = document.getElementById('color-btn-3');
 e.onclick = function () {
-    document.body.classList.add('e-theme');
+    document.body.classList.add('savane-theme');
     document.body.classList.remove('main-theme');
     document.body.classList.remove('ocean-theme');
     document.body.classList.remove('white-theme');
@@ -69,7 +69,7 @@ let white = document.getElementById('color-btn-4');
 white.onclick = function () {
     document.body.classList.add('white-theme');
     document.body.classList.remove('main-theme');
-    document.body.classList.remove('e-theme');
+    document.body.classList.remove('savane-theme');
     document.body.classList.remove('ocean-theme');
     document.body.classList.remove('discord-theme');
 }
@@ -78,7 +78,7 @@ let discord = document.getElementById('color-btn-5');
 discord.onclick = function () {
     document.body.classList.add('discord-theme');
     document.body.classList.remove('main-theme');
-    document.body.classList.remove('e-theme');
+    document.body.classList.remove('savane-theme');
     document.body.classList.remove('white-theme');
     document.body.classList.remove('ocean-theme');
 }
