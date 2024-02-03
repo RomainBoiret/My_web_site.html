a8fef6a8aa6bb3550e4edfa908768a912a696202
Romain <romainboiret@gmail.com>
2024-02-03 13:17:04 -0500

    some changes

--- scripts/script.js
+++ scripts/script.js
@@ -18,8 +18,8 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight - 100;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
